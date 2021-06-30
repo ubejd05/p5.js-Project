4088bb321d602faceb5cb5bd97b0428bd9de207f
Ubejd <ubed.haziri2005@gmail.com>
2021-06-30 12:16:02 +0200

    functionalized the number inputs

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-let circles = 50; // max 100, min 10
-let wave = 3; //  min 0
-let angle = 60;  // max 320, min 2
-let size = 4;  // max 5, min 1
-let speed = 3;  // max 20, min 0
-let rotation = 100;  // min 1, ///nese statike pakufi\\\, nese levizese max 500
+// let circles = 50; // max 100, min 10
+// let wave = 3; //  min 0
+// let angle = 60;  // max 320, min 2
+// let size = 4;  // max 5, min 1
+// let speed = 3;  // max 20, min 0
+// let rotation = 100;  // min 1, ///nese statike pakufi\\\, nese levizese max 500
 // let rrethiVogelMes = 0;  //1 ose 0
 
 // me  qit rrotullim statik edhe levizes
 // me qit rbg ose joRBG
+
+
+let circles = Number(document.getElementById("circles").value);
+let wave = Number(document.getElementById("wave").value);
+let angle = Number(document.getElementById("angle").value);
+let size = Number(document.getElementById("size").value);
+let speed = Number(document.getElementById("speed").value);
+let rotation = Number(document.getElementById("rotation").value)
+
+let inputs = document.querySelectorAll("input")
+let reset = document.getElementById("reset")
+
+reset.addEventListener("click", (event) =>{
+   location.reload()
+})
+// console.log(inputs);
+
+inputs.forEach((input) => {
+   input.addEventListener("change", (event) => {
+      circles = Number(document.getElementById("circles").value);
+      wave = Number(document.getElementById("wave").value);
+      angle = Number(document.getElementById("angle").value);
+      size = Number(document.getElementById("size").value);
+      speed = Number(document.getElementById("speed").value);
+      rotation = Number(document.getElementById("rotation").value);
+   });
+});
+
+
+
+
 
 let myCanvas = document.getElementById("myCanvas")
 // myCanvas.style.backgroundColor = "red";
